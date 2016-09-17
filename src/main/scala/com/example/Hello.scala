3ad0f7653c7b8fc72eee75058512f93b26dba49f
shigemk2 <i.am.shige@gmail.com>
2016-09-17 14:19:21 +0900

    Add ActorSystem

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,6 +1,13 @@
 package com.example
 
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val system = ActorSystem("mySystem")
+    implicit val timeout = Timeout(5 seconds)
   }
 }
