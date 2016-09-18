f911242565553712e607336e49c4ae99ffff3c8d
shigemk2 <i.am.shige@gmail.com>
2016-09-18 15:53:57 +0900

    Add Actor and Future

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,10 @@
 package com.example
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
 import akka.util.Timeout
+
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 
@@ -9,5 +12,11 @@ object Hello {
   def main(args: Array[String]): Unit = {
     val system = ActorSystem("mySystem")
     implicit val timeout = Timeout(5 seconds)
+
+    val props = Props[MyActor]
+    val actor = system.actorOf(props, name = "myActor")
+
+    val future1: Future[Any] = actor ? "hi"
+    val future2: Future[Any] = actor ? "hello"
   }
 }
